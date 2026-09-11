b379d5b88104f446433d822358cc5c4f137a5178
Omar Elgamal <omarelgamal0001@gmail.com>
2026-09-11 06:34:12 +0300

    add custom scroll view

--- lib/Features/Splash/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/Splash/presentation/views/widgets/book_details_view_body.dart
@@ -12,38 +12,45 @@ class BookDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
-    return Column(
-      children: [
-        const CustomBookDetailsAppBar(),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: width * .25, vertical: 20),
-          child: const FeaturedListViewItems(),
-        ),
-        SizedBox(height: 43),
-        const Text('The Jungle Book', style: Styles.title30),
-        SizedBox(height: 6),
-        Opacity(
-          opacity: .7,
-          child: Text('Rudyard Kipling', style: Styles.title18),
-        ),
-        const SizedBox(height: 18),
-        BookRating(mainAxisAlignment: MainAxisAlignment.center),
-        SizedBox(height: 5),
-        BookAction(),
-        const SizedBox(height: 25),
-        Align(
-          alignment: Alignment.centerLeft,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 12.0),
-            child: Text(
-              'You can also like',
-              style: Styles.title18.copyWith(fontWeight: FontWeight.w500),
+
+    return CustomScrollView(
+      slivers: [
+        Column(
+          children: [
+            const CustomBookDetailsAppBar(),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: width * .25,
+                vertical: 20,
+              ),
+              child: const FeaturedListViewItems(),
             ),
-          ),
+            SizedBox(height: 43),
+            const Text('The Jungle Book', style: Styles.title30),
+            SizedBox(height: 6),
+            Opacity(
+              opacity: .7,
+              child: Text('Rudyard Kipling', style: Styles.title18),
+            ),
+            const SizedBox(height: 18),
+            BookRating(mainAxisAlignment: MainAxisAlignment.center),
+            SizedBox(height: 5),
+            BookAction(),
+            Expanded(child: const SizedBox(height: 25)),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                child: Text(
+                  'You can also like',
+                  style: Styles.title18.copyWith(fontWeight: FontWeight.w500),
+                ),
+              ),
+            ),
+            const SimilarBooksListView(),
+          ],
         ),
-        const SimilarBooksListView(),
       ],
     );
   }
 }
-
